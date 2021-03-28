b8607f9929eca1b8f81f411c30305cb80b085c2e
Khalid Abuhakmeh <khalidabuhakmeh@gmail.com>
2021-03-28 18:41:34 -0400

    remove JavaScript from body

--- PutItInTurbo/wwwroot/js/site.js
+++ PutItInTurbo/wwwroot/js/site.js
@@ -2,3 +2,12 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
+document.addEventListener("turbolinks:request-end", function(e) {
+    console.log("request-end");
+    console.log(e);
+});
+
+document.addEventListener("turbolinks:visit", function(e) {
+    console.log(`visit`);
+    console.log(e);
+});
